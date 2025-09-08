ee77d9b9618ca542da54c06341fcc76b27db0c6c
Fernando Barrios <fernando@nika.agency>
2025-09-08 14:02:56 -0300

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  output: 'static',
+  vite: { plugins: [tailwindcss()] },
+});
